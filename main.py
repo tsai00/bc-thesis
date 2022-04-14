27fee44ea73330729e1a6398080ed800796e71e2
Ilya Tsakunov <i.tsakunov@student.fontys.nl>
2022-04-14 17:41:37 +0200

    Add helper functions

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from web_scraper import WebScraper
-
+import datetime
 
 if __name__ == '__main__':
-    pass
+    print('STARTED ON:', datetime.datetime.now())
+
+    webscraper = WebScraper(file_input='input.xlsx', file_used_ids='used_ids.xlsx', file_proxies='proxies.txt')
+
+    webscraper.run_scraper()
+
+    print('DONE ON:', datetime.datetime.now())
--- web_scraper.py
+++ web_scraper.py
@@ -1,5 +1,62 @@
+import pandas as pd
+
+from selenium import webdriver
+from selenium.webdriver import Proxy
+from selenium.webdriver.common.proxy import ProxyType
+
+
 class WebScraper:
 
-    def __init__(self):
-        pass
+    def __init__(self, file_input, file_used_ids, file_proxies):
+        self.input_file = file_input
+        self.file_used_ids = file_used_ids
+        self.file_proxies = file_proxies
+
+        self._used_ids = []     # List with IDs of previously scraped listings
+
+    @staticmethod
+    def add_column_with_links(input_data):
+        url_base = 'https://www.bezrealitky.cz/nemovitosti-byty-domy/'
+
+        input_data['url'] = input_data['uri'].apply(lambda x: url_base + x)
+
+        return input_data
+
+    def make_list_with_used_ids(self, file_used_ids):
+        df = pd.read_excel(file_used_ids)
+        self._used_ids.extend(df['id'].tolist())
+
+
+    @staticmethod
+    def get_proxy_list(proxies_file):
+        proxies = pd.read_table(proxies_file, delim_whitespace=True, header=None)[0].values.tolist()
+
+        return proxies
+
+    @staticmethod
+    def set_browser(proxy_input):
+
+        capabilities = webdriver.DesiredCapabilities.CHROME
+
+        # Set up proxy
+        if proxy_input is not None:
+            try:
+                proxy = Proxy()
+                proxy.proxy_type = ProxyType.MANUAL
+                proxy.http_proxy = proxy_input
+                proxy.ssl_proxy = proxy_input
+
+                proxy.add_to_capabilities(capabilities)
+            except:
+                pass
+
+        options = webdriver.ChromeOptions()
+
+        # Do not show browser GUI
+        options.add_argument('--headless')
+
+        driver = webdriver.Chrome(desired_capabilities=capabilities, options=options)
+
+        return driver
+
 
